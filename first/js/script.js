d09a84310a0adde9045e8b826a4cdc88269efa45
Oleg Kovalenko <abigor4202@gmail.com>
2018-06-25 15:26:03 +0300

    Initial commit

---
+++ first/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".carousel").carousel({
+    interval: 3000,
+    wrap: false
+    });
+    $("#exampleModal").modal({
+    keyboard: false;
+    backdrop: "static"
+    });
+    $(".container")fadeIn("slow", 5000);
+ });
+
